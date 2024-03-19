ea5483fa47a7f33d683473bb2e69de412842f6b1
qchris101 <qchris101@yahoo.com>
2024-03-19 12:57:18 -0400

    Completed Lesson, attached Code Challenge.

--- Methods/app.js
+++ Methods/app.js
@@ -43,3 +43,25 @@ const cat = {
 }
 
 const meow2 = cat.meow;
+
+// This keyword code practice:
+
+// Define an object called hen.  It should have three properties:
+// name should be set to 'Helen'
+// eggCount should be set to 0
+// layAnEgg should be a method which increments the value of eggCount by 1 and returns the string "EGG".  You'll need to use this.
+
+const hen = {
+    name : "Helen",
+    eggCount: 0,
+    layAnEgg(){
+        this.eggCount++ // This is referring to the eggcount key already created in the hen object
+        return "EGG"
+    }
+}
+
+hen.name // "Helen"
+hen.eggCount // 0
+hen.layAnEgg() // "EGG"
+hen.layAnEgg() // "EGG"
+hen.eggCount // 2
